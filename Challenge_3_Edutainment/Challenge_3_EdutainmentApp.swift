41e2841a514579a52f7825c36f38214d478fcbb3
Daniel Freire <dgz1606@gmail.com>
2023-12-25 09:14:17 -0500

    Initial Commit

---
+++ Challenge_3_Edutainment/Challenge_3_EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_3_EdutainmentApp.swift
+//  Challenge_3_Edutainment
+//
+//  Created by Daniel Freire on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_3_EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge_3_Edutainment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge_3_Edutainment
+//
+//  Created by Daniel Freire on 12/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
